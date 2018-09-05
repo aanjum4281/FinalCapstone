5f04a267924115f297a060ab1a91590a120f42d7
Amna Anjum <amnaanjum@Amnas-MacBook-Air.local>
2018-09-05 10:03:07 -0400

    first commit

---
+++ src/main/java/co/grandcircus/ApiCapstone/ApiCapstoneApplication.java
@@ -0,0 +1,12 @@
+package co.grandcircus.ApiCapstone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCapstoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCapstoneApplication.class, args);
+	}
+}
